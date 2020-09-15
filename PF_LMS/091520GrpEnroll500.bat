5f0181ca747bace95aef8821525fdcc7a27a78b1
MED16ica <mark.medley@peoplefluent.com>
2020-09-15 14:12:46 -0400

    Test Bat Files

---
+++ PF_LMS/091520GrpEnroll500.bat
@@ -0,0 +1,10 @@
+@echo on
+for /f "tokens=2 delims==" %%a in ('wmic OS Get localdatetime /value') do set "dt=%%a"
+set "YY=%dt:~2,2%" & set "YYYY=%dt:~0,4%" & set "MM=%dt:~4,2%" & set "DD=%dt:~6,2%"
+set "HH=%dt:~8,2%" & set "Min=%dt:~10,2%" & set "Sec=%dt:~12,2%"
+
+set timestamp=%MM%%DD%%YY%-%HH%%Min%%Sec%
+
+E:
+CD E:\JMETER\apache-jmeter-5.2.1\bin
+Start jmeter -n -t "E:\Performance Scripts\NETD\091420GE500.jmx" -l "E:\Performance Analysis\NETD\GrpEnroll500%timestamp%.csv" -e -o "E:\Performance HTML\NETD\GrpEnroll500%timestamp%"
